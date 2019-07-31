73a1572237bd787daefc72879dd809876753976e
Jmnoland <jody-noreply@git.com>
2019-07-31 18:22:17 +0200

    Initial commit

---
+++ scripts/FetchRentalsDue.py
@@ -0,0 +1,100 @@
+import os
+import json
+import datetime
+import threading
+import queue
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+rentalResults = []
+clientResults = []
+rentalsComplete = threading.Event()
+
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, '../environment/credentials.json')
+
+cred = credentials.Certificate(filename)
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+q = queue.Queue()
+
+def main():
+    date = datetime.datetime.today()
+    utcOffset = datetime.timedelta(hours=2, seconds=1)
+    start = date.replace(minute=0,hour=0,second=0,microsecond=0) - utcOffset
+    end = date.replace(minute=59,hour=23,second=59,microsecond=0) - utcOffset
+
+    thread = threading.Thread(target=queryRental, args=(start,end))
+    thread.start()
+    rentalsComplete.wait()
+
+    for doc in rentalResults:
+        q.put(doc["clientId"])
+
+    clientThread = threading.Thread(target=queryClient)
+    clientThread.start()
+
+    q.join()
+    formatData()
+    writeJson(date)
+
+# Function to query rentals due
+def queryRental(start, end):
+    rentalQuery = db.collection(u'rentals').where(u'billDate',u'>',start).where(u'billDate',u'<',end)
+    rentalQuery.on_snapshot(rentalFetch)
+
+# Function to query client with rental due
+def queryClient():
+    while (q.empty() != True):
+        ref = q.get()
+        ref.on_snapshot(clientFetch)
+
+# Function called after callback received
+def rentalFetch(col_snapshot, changes, read_time):
+    print(u'Callback received query snapshot at:  ', read_time)
+    data = None
+    for doc in col_snapshot:
+        data = doc.to_dict()
+        data['rentalId'] = doc.id
+        rentalResults.append(data)
+    rentalsComplete.set()
+
+# Function called after callback received
+def clientFetch(col_snapshot, changes, read_time):
+    print(u'Callback received query snapshot at:  ', read_time)
+    data = None
+    for doc in col_snapshot:
+        data = doc.to_dict()
+        data["clientId"] = doc.id
+        clientResults.append(data)
+        q.task_done()
+
+# Format data to include rentals for each client within client list
+def formatData():
+    for client in range(len(clientResults)):
+        clientResults[client]["rentals"] = []
+        for rental in range(len(rentalResults)):
+            id = None
+            try:
+                id = (rentalResults[rental]["clientId"].path).split("/")
+            except AttributeError:
+                id = rentalResults[rental]["clientId"]
+            if(clientResults[client]["clientId"] == id[1]):
+                rentalResults[rental]["clientId"] = id[1]
+                rentalResults[rental]["startDate"] = rentalResults[rental]["startDate"].strftime("%d/%m/%Y, %H:%M:%S")
+                rentalResults[rental]["billDate"] = rentalResults[rental]["billDate"].strftime("%d/%m/%Y, %H:%M:%S")
+                rentalResults[rental]["endDate"] = rentalResults[rental]["endDate"].strftime("%d/%m/%Y, %H:%M:%S")
+                rentalResults[rental]["assetId"] = (rentalResults[rental]["assetId"].path).split("/")[1]
+                clientResults[client]["rentals"].append(rentalResults[rental])
+                id = None
+                break
+
+def writeJson(date):
+    fileName = date.strftime("%d-%m-%Y") + '.json'
+    dirname = os.path.dirname(__file__)
+    finalName = os.path.join(dirname, '../files/json/' + fileName)
+    with open(finalName, 'w', encoding='utf-8') as file:
+        json.dump(clientResults, file, ensure_ascii=False, indent=4)
---
+++ scripts/GenerateRentalPDF.py
@@ -0,0 +1,121 @@
+import os
+import datetime
+import json
+
+from fpdf import FPDF
+
+def main():
+    dirname = os.path.dirname(__file__)
+    folder = os.path.join(dirname, '../files/json/')
+    fileList = os.listdir(folder)
+
+    accountDetails = []
+    bDetails = os.path.join(os.path.dirname(__file__), '../environment/accountInfo.json')
+    with open(bDetails) as bank_file:
+        accountDetails = json.load(bank_file)
+
+    for json_file in fileList:
+        path = os.path.join(os.path.dirname(__file__), '../files/json/' + json_file)
+        with open(path) as file:
+            data = json.load(file)
+            for client in data:
+                makePDF(client, accountDetails)
+                makeJSON(client)
+        os.remove(path)
+
+def makePDF(client, accountDetails):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Arial', 'B', 10)
+    pdf.cell(0, 10, 'Economusic')
+    
+    pdf.set_font('Arial', 'B', size=20)
+    pdf.cell(0, 10, 'Rental Invoice', 0, 0, "R")
+    pdf.ln()
+    
+    pdf.set_font("Arial", size=9)
+    dateText = datetime.datetime.today().strftime("%b %d, %Y")
+    fullText = "# Rental Invoice " + dateText
+    pdf.cell(0, 5, fullText, 0, 0, "R")
+    pdf.ln()
+    pdf.cell(0, 5, (client["name"] + " " + client["surname"]), 0, 0, "R")
+    pdf.ln()
+    pdf.ln()
+
+    pdf.cell(125, 5, "Bill To: ")
+    pdf.cell(0, 5, "Date:")
+    pdf.cell(0, 5, dateText, 0, 0, "R")
+    pdf.ln()
+
+    pdf.set_font('Arial', 'B', 11)
+    pdf.cell(125, 5, (client["name"] + " " + client["surname"]))
+    pdf.set_font("Arial", size=9)
+    pdf.cell(0, 5, "Payment Terms:")
+    pdf.cell(0, 5, "EFT", 0, 0, "R")
+    pdf.ln()
+
+    pdf.cell(125, 5, "")
+    pdf.cell(0, 5, "Due Date:")
+    pdf.cell(0, 5, "???", 0, 0, "R")
+    pdf.ln()
+
+    pdf.set_fill_color(220,220,220)
+    pdf.set_font("Arial", "B", 11)
+    pdf.cell(125, 7, "")
+    pdf.cell(0, 7, "Balance Due:", 0, 0, 0, True)
+    assets = []
+    total = 0
+    for rental in client["rentals"]:
+        total = total + rental["rent"]
+        assets.append({"name": rental["assetName"], "qty": 1, "rent": rental["rent"]})
+
+    pdf.cell(0, 7, ("R " + str(total)), 0, 0, "R")
+    pdf.ln()
+    pdf.ln()
+
+    pdf.set_fill_color(0, 0, 0)
+    pdf.set_text_color(255, 255, 255)
+    pdf.cell(98, 7, "Item", "LTB", 0, 0, True)
+    pdf.cell(40, 7, "Quantity", "TB", 0, 0, True)
+    pdf.cell(32, 7, "Rate", "TB", 0, 0, True)
+    pdf.cell(20, 7, "Amount", "TRB", 0, 0, True)
+    pdf.ln()
+    
+    pdf.set_font("Arial", size=11)
+    pdf.set_text_color(0, 0, 0)
+    for asset in assets:
+        pdf.cell(98, 6, asset["name"])
+        pdf.cell(40, 6, str(asset["qty"]))
+        pdf.cell(32, 6, "R " + str(asset["rent"]))
+        pdf.cell(20, 6, "R " + str(asset["rent"]))
+        pdf.ln()
+    pdf.ln()
+    pdf.ln()
+
+    pdf.cell(125, 5, "")
+    pdf.cell(0, 5, "Subtotal:")
+    pdf.cell(0, 5, "R " + str(total), 0, 0, "R")
+    pdf.ln()
+
+    pdf.cell(125, 5, "")
+    pdf.cell(0, 5, "Total:")
+    pdf.cell(0, 5, "R " + str(total), 0, 0, "R")
+    pdf.ln()
+
+    pdf.cell(0, 10, "Notes")
+    pdf.ln()
+    pdf.cell(0, 5, accountDetails["name"])
+    pdf.ln()
+    pdf.cell(0, 5, accountDetails["bank"])
+    pdf.ln()
+    pdf.cell(0, 5, accountDetails["num"])
+    pdf.ln()
+
+    final = os.path.join(os.path.dirname(__file__), '../files/batched/' + client["clientId"] + '.pdf')
+    pdf.output(final, 'F')
+
+def makeJSON(client):
+    finalName = os.path.join(os.path.dirname(__file__), '../files/batched/' + client["clientId"] + '.json')
+    with open(finalName, 'w', encoding='utf-8') as file:
+        json.dump(client, file, ensure_ascii=False, indent=4)
+main()
