037eb65d1317448d5c9f623e07c69f11fab7d7e5
Jmnoland <jody-noreply@git.com>
2020-03-14 19:04:09 +0200

    Tracking jobs with db

--- scripts/Scheduler.py
+++ scripts/Scheduler.py
@@ -35,7 +35,7 @@ class Scheduler():
                         self.__cur.execute("""CREATE TABLE IF NOT EXISTS Jobs (
                                         Process TEXT NOT NULL,
                                         Complete BIT NOT NULL,
-                                        Runtime TEXT NOT NULL
+                                        Runtime TIMESTAMP NOT NULL
                                 );""")
                         self.conn.commit()
                 except Exception:
@@ -55,38 +55,37 @@ class Scheduler():
                 scheduler = BlockingScheduler(executors=executors, job_defaults=job_defaults, timezone=utc)
                 
                 self.createJobs(scheduler)
-                
                 scheduler.start()
+                
+        def readJobs(self):
+                self.__cur.execute('''SELECT * FROM Jobs ORDER BY Runtime''')
+                for row in self.__cur:
+                        print(row)
+
+        def delete(self):
+                self.__cur.execute("DELETE FROM Jobs")
+                self.conn.commit()
 
         def createJobs(self, scheduler):
-                self.__cur.execute('''SELECT * FROM Jobs WHERE Complete = 0 AND strftime("%d-%m-%Y %H", Runtime) < ? ORDER BY Runtime''', datetime.datetime.today())
-                create = {
-                        "fetch": True,
-                        "generate": True,
-                        "complete": True
-                }
-                for job in self.__cur:
+                now = datetime.datetime.today()
+                self.__cur.execute('''SELECT * FROM Jobs 
+                                WHERE Complete = 0 AND Runtime < ? 
+                                ORDER BY Runtime''', (now,))
+                pastJobs = [row for row in self.__cur]
+
+                for job in pastJobs:
+                        print(job)
                         if job[0] == 'FetchRentals':
                                 self.fetchRentals()
-                                create["fetch"] = False
                         if job[0] == 'GenerateRentalPDF':
                                 self.generatePDF()
-                                create["generate"] = False
                         if job[0] == 'Complete':
                                 self.email()
-                                create["complete"] = False
-                
-                if create["fetch"]:
-                        self.__cur.execute("INSERT INTO Jobs VALUES (?,?,?)", ("FetchRentals", 0, datetime.datetime.today()))
-                if create["generate"]:
-                        self.__cur.execute("INSERT INTO Jobs VALUES (?,?,?)", ("GenerateRentalPDF", 0, datetime.datetime.today()))
-                if create["complete"]:
-                        self.__cur.execute("INSERT INTO Jobs VALUES (?,?,?)", ("Complete", 0, datetime.datetime.today()))
-                self.conn.comit()
                 
                 scheduler.add_job(self.email, 'cron', hour='8')
                 scheduler.add_job(self.generatePDF, 'cron', hour='14')
                 scheduler.add_job(self.fetchRentals, 'cron', hour='12')
+                scheduler.add_job(self.dbMaintenance, 'cron', day_of_week=6)
                
         def checkRun(self):
                 check_ref = self.db.collection(u'check').document(u'0')
@@ -97,8 +96,8 @@ class Scheduler():
                 if(self.checkRun() == True):
                         try:
                                 FetchRentalsDue.main()
-                                self.__cur('''UPDATE Jobs SET Complete = 1 WHERE Complete = 0 AND Process LIKE "FetchRentals"''')
-                                self.__cur.execute("INSERT INTO Jobs VALUES (?,?,?)", ("FetchRentals", 0, datetime.datetime.today()))
+                                self.__cur.execute(''' UPDATE Jobs SET Complete = 1 WHERE Complete = 0 AND Process LIKE 'FetchRentals' ''')
+                                self.__cur.execute("INSERT INTO Jobs VALUES (?,?,?)", ("FetchRentals", False, self.newDateTime(datetime.datetime.today(), 12)))
                                 self.conn.commit()
                         except Exception:
                                 self.stackTrace = traceback.format_exc()
@@ -110,8 +109,8 @@ class Scheduler():
                 if(self.checkRun() == True):
                         try:
                                 GenerateRentalPDF.main()
-                                self.__cur('''UPDATE Jobs SET Complete = 1 WHERE Complete = 0 AND Process LIKE "GenerateRentalPDF"''')
-                                self.__cur.execute("INSERT INTO Jobs VALUES (?,?,?)", ("GenerateRentalPDF", 0, datetime.datetime.today()))
+                                self.__cur.execute(''' UPDATE Jobs SET Complete = 1 WHERE Complete = 0 AND Process LIKE 'GenerateRentalPDF' ''')
+                                self.__cur.execute("INSERT INTO Jobs VALUES (?,?,?)", ("GenerateRentalPDF", False, self.newDateTime(datetime.datetime.today(), 14)))
                                 self.conn.commit()
                         except Exception:
                                 self.stackTrace = traceback.format_exc()
@@ -123,14 +122,24 @@ class Scheduler():
                 if(self.checkRun() == True):
                         try:
                                 Complete.main()
-                                self.__cur('''UPDATE Jobs SET Complete = 1 WHERE Complete = 0 AND Process LIKE "Complete"''')
-                                self.__cur.execute("INSERT INTO Jobs VALUES (?,?,?)", ("Complete", 0, datetime.datetime.today()))
+                                self.__cur.execute(''' UPDATE Jobs SET Complete = 1 WHERE Complete = 0 AND Process LIKE 'Complete' ''')
+                                self.__cur.execute("INSERT INTO Jobs VALUES (?,?,?)", ("Complete", False, self.newDateTime(datetime.datetime.today(), 8)))
                                 self.conn.commit()
                         except Exception:
                                 self.stackTrace = traceback.format_exc()
                                 if(self.stackTrace == None):
                                         self.stackTrace = "No error"
                                 self.errorHandler("Complete", self.stackTrace)
+        
+        def dbMaintenance(self):
+                clearDate = datetime.datetime.today()
+                clearDate = clearDate.replace(month=(clearDate.month - 2))
+                self.__cur.execute(''' DELETE FROM Jobs WHERE Runtime < ? ''', (clearDate,))
+                self.conn.commit()
+
+        def newDateTime(self, dtObj, nHour, addDay=1):
+                newDate = dtObj + datetime.timedelta(days=addDay)
+                return newDate.replace(hour=nHour, minute=0, second=0, microsecond=0)
                         
         def errorHandler(self, process, error):
                 errorInfo = []
@@ -151,3 +160,4 @@ class Scheduler():
                 errorInfo.append({ "process": process, "errorMessage": error})
                 with open(os.path.join(directoryName, errorPath + "/errors.json"), 'w') as json_file:
                         json.dump(errorInfo, json_file)
+Scheduler()
